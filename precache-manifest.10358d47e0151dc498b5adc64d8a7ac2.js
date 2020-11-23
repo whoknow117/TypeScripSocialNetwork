6431211e7ec82cdecb9eae495f3f272e6801b0d2
newbeeguy <mskjavascript@gmail.com>
2020-11-23 23:34:18 +0300

    Updates

---
+++ precache-manifest.10358d47e0151dc498b5adc64d8a7ac2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d329bd29734ab5ab4ba733744545238d",
+    "url": "/TypeScriptSocialNetwork/index.html"
+  },
+  {
+    "revision": "3a66347b1056e421770a",
+    "url": "/TypeScriptSocialNetwork/static/css/main.97a28f59.chunk.css"
+  },
+  {
+    "revision": "eb123eecda9e5055ccea",
+    "url": "/TypeScriptSocialNetwork/static/js/2.5c71d3cf.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/TypeScriptSocialNetwork/static/js/2.5c71d3cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a66347b1056e421770a",
+    "url": "/TypeScriptSocialNetwork/static/js/main.31a064fd.chunk.js"
+  },
+  {
+    "revision": "3b78611265c4cfa2cbbd",
+    "url": "/TypeScriptSocialNetwork/static/js/runtime-main.6dd7237e.js"
+  }
+]);
