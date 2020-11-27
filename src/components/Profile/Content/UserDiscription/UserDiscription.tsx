88e763b29d22702b40c311a9da080b705f528ae1
newbeeguy <mskjavascript@gmail.com>
2020-11-27 19:55:01 +0300

    profilePage ui

--- src/components/Profile/Content/UserDiscription/UserDiscription.tsx
+++ src/components/Profile/Content/UserDiscription/UserDiscription.tsx
@@ -9,8 +9,29 @@ const UserDiscription: React.FC<UserDiscriptionType> = () => {
 
     return <div className={classes.userDiscription}>
         <h3 className={classes.userTitle}>Какойто Челик</h3>
-        <p className={classes.text}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aperiam consequuntur ducimus natus obcaecati rem voluptas. Ab autem illum incidunt inventore mollitia repellendus sequi veritatis.</p>
-        <ul className={classes.list}>
+        <p className={classes.text}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aperiam
+            consequuntur ducimus natus obcaecati rem voluptas. Ab autem illum incidunt inventore mollitia repellendus
+            sequi veritatis.</p>
+        <div className={` ${classes.top}`}>
+            <ul className={classes.list}>
+
+                <li className={classes.item}>Пол:</li>
+                <li className={classes.item}>День рождения:</li>
+                <li className={classes.item}>Город:</li>
+                <li className={classes.item}>Место работы:</li>
+
+            </ul>
+            <ul className={classes.list}>
+
+                <li className={classes.answer}>мужской</li>
+                <li className={classes.answer}>3 сентября 1990</li>
+                <li className={classes.answer}>Северодонецк</li>
+                <li className={classes.answer}>Магазин "Снабженец"</li>
+
+            </ul>
+        </div>
+
+        <div className={classes.table}>
             <div className={classes.lineWrapp}>
                 <span className={classes.itemTitle}>
                     Общая информация
@@ -20,79 +41,84 @@ const UserDiscription: React.FC<UserDiscriptionType> = () => {
 
                     </span>
             </div>
-            <li className={classes.item}>пол: <span className={classes.answer}>муж</span></li>
-            <li className={classes.item}>день рождения: <span className={classes.answer}>33года</span></li>
-            <li className={classes.item}>места учебы: <span className={classes.answer}>СТИ</span></li>
-            <li className={classes.item}>увлечения: <span className={classes.answer}>игры, фильмы, музыка</span></li>
-            <li className={classes.item}>место работы: <span className={classes.answer}>магазин Снабженец</span></li>
+            <div className={classes.separate}>
+                <ul className={classes.list}>
 
-        </ul>
-        <ul className={classes.list}>
+                    <li className={classes.item}>Место учебы:</li>
+                    <li className={classes.item}>Увлечения:</li>
+
+                </ul>
+                <ul className={classes.list}>
+
+                    <li className={classes.answer}>СТИ</li>
+                    <li className={classes.answer}>игры, фильмы, музыка</li>
+
+                </ul>
+            </div>
+
+        </div>
+        <div className={classes.table}>
             <div className={classes.lineWrapp}>
                 <span className={classes.itemTitle}>
-                    Общая информация
+                    Предпочтения
 
                 </span>
                 <span className={classes.border}>
 
                     </span>
             </div>
-            <li className={classes.item}>пол: <span className={classes.answer}>муж</span></li>
-            <li className={classes.item}>день рождения: <span className={classes.answer}>33года</span></li>
-            <li className={classes.item}>места учебы: <span className={classes.answer}>СТИ</span></li>
-            <li className={classes.item}>увлечения: <span className={classes.answer}>игры, фильмы, музыка</span></li>
-            <li className={classes.item}>место работы: <span className={classes.answer}>магазин Снабженец</span></li>
-        </ul>
-        <ul className={classes.list}>
+            <div className={classes.separate}>
+                <ul className={classes.list}>
+
+                    <li className={classes.item}>Политические взгляды:</li>
+                    <li className={classes.item}>Отношение к курениею:</li>
+                    <li className={classes.item}>Отношение к алкоголю:</li>
+                    <li className={classes.item}>Главное в людях:</li>
+
+
+                </ul>
+                <ul className={classes.list}>
+
+                    <li className={classes.answer}>Пихуй</li>
+                    <li className={classes.answer}>Нейтральное</li>
+                    <li className={classes.answer}>Негативное</li>
+                    <li className={classes.answer}>Честность, пунктуальность, хорошее чувство юмора</li>
+
+
+                </ul>
+            </div>
+
+        </div>
+        <div className={classes.table}>
             <div className={classes.lineWrapp}>
                 <span className={classes.itemTitle}>
-                    Общая информация
+                    Карьера
 
                 </span>
                 <span className={classes.border}>
 
                     </span>
             </div>
-            <li className={classes.item}>пол: <span className={classes.answer}>муж</span></li>
-            <li className={classes.item}>день рождения: <span className={classes.answer}>33года</span></li>
-            <li className={classes.item}>места учебы: <span className={classes.answer}>СТИ</span></li>
-            <li className={classes.item}>увлечения: <span className={classes.answer}>игры, фильмы, музыка</span></li>
-            <li className={classes.item}>место работы: <span className={classes.answer}>магазин Снабженец</span></li>
-        </ul>
-        <ul className={classes.list}>
-            <div className={classes.lineWrapp}>
-                <span className={classes.itemTitle}>
-                    Общая информация
+            <div className={classes.separate}>
+                <ul className={classes.list}>
 
-                </span>
-                <span className={classes.border}>
+                    <li className={classes.item}>Места учебы:</li>
+                    <li className={classes.item}>Увлечения:</li>
 
-                    </span>
+                </ul>
+                <ul className={classes.list}>
+
+                    <li className={classes.answer}>СТИ</li>
+                    <li className={classes.answer}>игры, фильмы, музыка</li>
+
+                </ul>
             </div>
-            <li className={classes.item}>пол: <span className={classes.answer}>муж</span></li>
-            <li className={classes.item}>день рождения: <span className={classes.answer}>33года</span></li>
-            <li className={classes.item}>места учебы: <span className={classes.answer}>СТИ</span></li>
-            <li className={classes.item}>увлечения: <span className={classes.answer}>игры, фильмы, музыка</span></li>
-            <li className={classes.item}>место работы: <span className={classes.answer}>магазин Снабженец</span></li>
-        </ul>
-        <ul className={classes.list}>
-            <div className={classes.lineWrapp}>
-                <span className={classes.itemTitle}>
-                    Общая информация
 
-                </span>
-                <span className={classes.border}>
+        </div>
 
-                    </span>
-            </div>
-            <li className={classes.item}>пол: <span className={classes.answer}>муж</span></li>
-            <li className={classes.item}>день рождения: <span className={classes.answer}>33года</span></li>
-            <li className={classes.item}>места учебы: <span className={classes.answer}>СТИ</span></li>
-            <li className={classes.item}>увлечения: <span className={classes.answer}>игры, фильмы, музыка</span></li>
-            <li className={classes.item}>место работы: <span className={classes.answer}>магазин Снабженец</span></li>
-        </ul>
+        </div>
 
-    </div>
+
 }
 
 
